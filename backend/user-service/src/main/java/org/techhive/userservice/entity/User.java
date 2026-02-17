85862e53946bc954cc65d8396909e955be06e9e3
Nessimbns2 <nessimbns2@gmail.com>
2026-02-17 21:00:47 +0100

    fix stuff

--- backend/user-service/src/main/java/org/techhive/userservice/entity/User.java
+++ backend/user-service/src/main/java/org/techhive/userservice/entity/User.java
@@ -1,10 +1,12 @@
 package org.techhive.userservice.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
 
   @Id
