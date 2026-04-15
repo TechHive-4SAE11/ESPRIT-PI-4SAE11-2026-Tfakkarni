1efcaeb7c26edc798987f6c9bdda0eb8cb0593ec
Aymen Hmani <aymen.hmani.tech@gmail.com>
2026-04-15 11:38:24 +0100

    fixed bugs

--- backend/medical-service/src/main/java/org/techhive/medicalservice/client/GameServiceClient.java
+++ backend/medical-service/src/main/java/org/techhive/medicalservice/client/GameServiceClient.java
@@ -1,13 +1,17 @@
 package org.techhive.medicalservice.client;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.techhive.medicalservice.dto.game.GameStatsDTO;
 
-@FeignClient(name = "game-service", path = "/api/game")
+@FeignClient(name = "game-service", path = "/api/games")
 public interface GameServiceClient {
 
     @GetMapping("/stats/patient/{patientId}")
     GameStatsDTO getPatientGameStats(@PathVariable("patientId") String patientId);
+
+    @GetMapping("/stats/analytics/{patientId}")
+    JsonNode getPatientAnalytics(@PathVariable("patientId") String patientId);
 }
