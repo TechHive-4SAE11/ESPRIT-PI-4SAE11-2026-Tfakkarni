dbb5820c1ac8e41e05bd5487fd2a0ecce0219f74
Aymen Hmani <aymen.hmani.tech@gmail.com>
2026-04-29 23:11:08 +0100

    fix: add missing EnableAutoConfiguration imports in test files

--- backend/api-gateway/src/test/java/org/techhive/apigateway/ApiGatewayApplicationTests.java
+++ backend/api-gateway/src/test/java/org/techhive/apigateway/ApiGatewayApplicationTests.java
@@ -1,10 +1,9 @@
 package org.techhive.apigateway;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
